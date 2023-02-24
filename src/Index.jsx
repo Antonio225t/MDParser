c14e565e4c94191f89c43a1658996e7dea4dccdf
Antonio225t <a.s25@live.it>
2023-02-24 03:20:06 +0100

    Fixed the editor showing 404 in GH-Pages.

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import Editor from './pages/editor/App'
@@ -6,13 +6,13 @@ import OnTheRight from './pages/on_the_right/App';
 
 function Index() {
     return (
-        <BrowserRouter>
+        <HashRouter baseline='/MDParser'>
             <Routes>
-                <Route exact path="/MDParser" element={<App />} />
-                <Route path='/MDParser/editor' element={<Editor />} />
-                <Route path='/MDParser/on_the_right' element={<OnTheRight />} />
+                <Route exact path="/" element={<App />} />
+                <Route path='/editor' element={<Editor />} />
+                <Route path='/on_the_right' element={<OnTheRight />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
